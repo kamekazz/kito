f37363d5269e9b4317958e91ba30ecf3011c1da4
kamekazz <taveras78@gmail.com>
2022-06-19 15:20:44 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# 4. Pass the data back to the main.py file, so that you can print the data from main.py
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "EWR"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+# for destination in sheet_data:
+#     flight = flight_search.check_flights(
+#         ORIGIN_CITY_IATA,
+#         destination["iataCode"],
+#         from_time=tomorrow,
+#         to_time=six_month_from_today
+#     )
+
+flight = flight_search.check_flights(
+    ORIGIN_CITY_IATA,
+    "STI",
+    from_time=tomorrow,
+    to_time=six_month_from_today
+)
